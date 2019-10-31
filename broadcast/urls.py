e56a21122708f71c066429adf785ee5fb63ceb5e
lindajoy <wawirajoy98@gmail.com>
2019-10-31 23:16:40 +0300

    first commit

---
+++ broadcast/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.conf.urls import url                                                                                                                                                         
+from . import views
+
+urlpatterns = [ 
+    url(r'broadcast$', views.broadcast_sms, name="default"),
+]
+
